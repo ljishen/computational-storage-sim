389a51784a5ad408fa31fd53a26eb07adc1a38cd
Jianshen Liu <jliu120@ucsc.edu>
2020-03-20 18:11:58 -0700

    Complete the functionality for read offloading

--- src/computational_storage_sim.py
+++ src/computational_storage_sim.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from abc import ABC
 from collections import namedtuple
+from functools import wraps
+from os import environ
 from random import SystemRandom
 
 import simpy
-
-from loguru import logger
 
 MICROSECOND = 1
 
@@ -19,32 +20,61 @@ class NetworkFabric:
         self.cable = simpy.Container(env,
                                      capacity=self._NETWORK_BW_GBPS * 1e9 / 8)
 
-        # host memory, assuming it is big enough to host any data
-        self.memory = simpy.Store(env)
+        self.endpoints = {}
 
-    def send(self, data_packet):
-        # yield self.cable.put(data_packet.length)
+    def connect(self, name, endpoint):
+        if not issubclass(type(endpoint), Memorable):
+            raise RuntimeError(
+                "{} is not a memorable endpoint".format(endpoint))
+
+        self.endpoints[name] = endpoint
+
+    def send(self, endpoint_name, data_packet):
+        if endpoint_name not in self.endpoints:
+            raise RuntimeError(
+                "network fabric has no connection to '{}'".format(
+                    endpoint_name))
+
+        yield self.cable.put(data_packet.length)
+
         yield self.env.timeout(data_packet.length / self.cable.capacity * 1e6)
-
-        # write data to memory
-        # self.memory.put(data_packet)
-
-    def receive(self):
-        # read from local memory
-        data_packet = yield self.memory.get()
         yield self.cable.get(data_packet.length)
-        return data_packet
+
+        self.endpoints[endpoint_name].memorize(data_packet.command_id,
+                                               data_packet)
 
 
-class EmbeddedPlatform:
+class Memorable(ABC):
+    def __init__(self):
+        self.cache = {}
+
+    def memorize(self, key, data):
+        self.cache[key] = data
+
+    def read_cache(self, key):
+        return self.cache.pop(key)
+
+
+# pylint: disable=too-few-public-methods
+class EmbeddedPlatform(Memorable):
+    ENDPOINT_NAME = "embedded"
+
     _NUM_PROCESSORS_EMBEDDED = 2
     _DATA_ACCESS_DELAY_PER_BYTE_EMBEDDED = MICROSECOND
     _COMPUTE_DELAY_PER_BYTE_EMBEDDED = MICROSECOND * 2
 
     def __init__(self, env, network_fabric, submission_queues,
                  completion_queues):
+        super().__init__()
+
+        self.logger = logger.bind(component=self.ENDPOINT_NAME)
+
         self.env = env
+
         self.network_fabric = network_fabric
+        network_fabric.connect(self.ENDPOINT_NAME, self)
+        self.logger.debug("connected to network fabric")
+
         self.submission_queues = submission_queues
         self.completion_queues = completion_queues
 
@@ -66,7 +96,8 @@ class EmbeddedPlatform:
             ret_length = comm.length * (1 - comm.selectivity)
 
         yield self.env.process(
-            self.network_fabric.send(DataPacket(comm.id, ret_length)))
+            self.network_fabric.send(HostPlatform.ENDPOINT_NAME,
+                                     DataPacket(comm.id, ret_length)))
 
         # place the CompletionCommand on the completion queue
         # of the same core
@@ -76,8 +107,7 @@ class EmbeddedPlatform:
                                       submit_timestamp=comm.timestamp)
         yield completion_queue.put(comp_comm)
 
-        logger.bind(platform="embedded",
-                    by_processor=processor_id).success(comp_comm)
+        self.logger.bind(by_processor=processor_id).trace(comp_comm)
 
     def __process_command(self, submission_queue, completion_queue):
         while True:
@@ -103,10 +133,11 @@ class EmbeddedPlatform:
                 self.__process_command(self.submission_queues[idx],
                                        self.completion_queues[idx]))
 
-        logger.debug("embedded platform has started")
+        self.logger.debug("platform has started")
 
 
-class HostPlatform:
+class HostPlatform(Memorable):
+    ENDPOINT_NAME = "host"
     NUM_PROCESSORS_HOST = 24
 
     _COMPUTE_DELAY_PER_BYTE_HOST = MICROSECOND
@@ -115,8 +146,15 @@ class HostPlatform:
     _IO_COMPLETION_Q_SIZE_PER_CORE = 64 * 1e3
 
     def __init__(self, env, network_fabric):
+        super().__init__()
+
+        self.logger = logger.bind(component=self.ENDPOINT_NAME)
+
         self.env = env
+
         self.network_fabric = network_fabric
+        network_fabric.connect(self.ENDPOINT_NAME, self)
+        self.logger.debug("connected to network fabric")
 
         self.submission_queues = []
         self.completion_queues = []
@@ -126,16 +164,10 @@ class HostPlatform:
             with self.completion_queues[processor_id].get() as slot:
                 comm = yield slot
                 if not comm.ret_isp:
-                    # need to read data from NetworkFabric
-                    yield self.env.timeout(comm.length *
+                    data_packet = self.read_cache(comm.id)
+                    yield self.env.timeout(data_packet.length *
                                            self._COMPUTE_DELAY_PER_BYTE_HOST)
-                    logger.bind(platform="host",
-                                by_processor=processor_id).success(comm)
-
-    # def __poll_data(self, network_fabric):
-    #     logger.debug('waiting to receive network data...')
-    #     while True:
-    #         network_fabric.receive()
+                    self.logger.bind(by_processor=processor_id).trace(comm)
 
     def start(self):
         for idx in range(self.NUM_PROCESSORS_HOST):
@@ -148,8 +180,8 @@ class HostPlatform:
 
             self.env.process(self.__process_command(idx))
 
-        logger.debug("host platform has started with established " +
-                     "submission_queues and completion_queues")
+        self.logger.debug("platform has started with established " +
+                          "submission_queues and completion_queues")
 
     def get_submission_queues(self):
         return self.submission_queues
@@ -158,16 +190,18 @@ class HostPlatform:
         return self.completion_queues
 
 
+# pylint: disable=too-few-public-methods
 class App:
     def __init__(self, env, submission_queues):
         self.env = env
         self.submission_queues = submission_queues
 
         self.cryptogen = SystemRandom()
-        self.counter = 0
 
-    def __start(self, app_idx):
-        for comm_idx in range(self.cryptogen.randrange(900) + 100):
+    def __start(self, app_idx, comms_per_app):
+        for comm_idx in range(
+                self.cryptogen.randrange(comms_per_app[1] - comms_per_app[0]) +
+                comms_per_app[0]):
             submit_comm = SubmissionCommand(
                 "{:d}-{:d}".format(app_idx, comm_idx), 4 * 1024, False, 0,
                 self.env.now)
@@ -175,12 +209,85 @@ class App:
                 app_idx % HostPlatform.NUM_PROCESSORS_HOST]
             yield submission_queue.put(submit_comm)
 
-    def start_batch(self, num):
+    def start_batch(self, num, comms_per_app=(500, 1000)):
+        """
+        Start a number of apps to generate submission commands.
+
+        Args:
+        ----
+            num (int): The number of apps to start.
+            comms_per_app: (int, int):
+                The number of commands in this range an app will generate.
+                If (a, b) is the range, the number of commands to generate
+                will be a <= x < b.
+
+        """
         for app_idx in range(num):
-            self.env.process(self.__start(app_idx))
+            self.env.process(self.__start(app_idx, comms_per_app))
 
 
-@logger.catch
+# pylint: disable=too-few-public-methods
+class EventTracer:
+    def __init__(self, env):
+        self.env = env
+        self.trace_data = []
+
+    def trace(self):  # noqa
+        """Replace the ``step()`` method of *env* with a tracing function
+        that calls *callbacks* with an events time, priority, ID and its
+        instance just before it is processed.
+
+        """
+
+        # pylint: disable=protected-access,invalid-name
+        def get_wrapper(env_step):
+            """Generate the wrapper for env.step()."""
+            @wraps(env_step)
+            def tracing_step():  # noqa
+                """Call *__monitor* for the next event if one exist before
+                calling ``env.step()``."""
+                if len(self.env._queue) > 0:
+                    t, prio, eid, event = self.env._queue[0]
+                    self.__monitor(t, prio, eid, event)
+                return env_step()
+
+            return tracing_step
+
+        self.env.step = get_wrapper(self.env.step)
+
+    def __monitor(self, t, prio, eid, event):
+        # pylint: disable=invalid-name,unused-argument
+        if isinstance(event, simpy.resources.store.StoreGet):
+            self.trace_data.append(
+                (t, eid, event.proc, type(event), event.resource.items[0]
+                 if len(event.resource.items) > 0 else None,
+                 event.resource.capacity))
+        elif isinstance(event, simpy.resources.store.StorePut):
+            self.trace_data.append((t, eid, event.proc, type(event),
+                                    event.item, event.resource.capacity))
+        else:
+            self.trace_data.append((
+                t,
+                eid,
+                event.proc if hasattr(event, 'proc') else None,
+                type(event),
+                event,
+            ))
+
+    def print_trace_data(self, tag="new"):
+        print(("==================== "
+               "TRACE DATA (BEGIN) (tag: {}) "
+               "====================").format(tag))
+        for data in self.trace_data:
+            print(data)
+        print(("==================== "
+               "TRACE DATA  (END)  (tag: {}) "
+               "====================\n").format(tag))
+
+
+TRACER = None
+
+
 def main():
     env = simpy.Environment()
 
@@ -194,16 +301,39 @@ def main():
                                          host_platform.get_completion_queues())
     embedded_platform.start()
 
-    App(env, host_platform.get_submission_queues()).start_batch(1)
+    App(env, host_platform.get_submission_queues()).start_batch(5, (300, 500))
+
+    # event tracing
+    ###############################
+
+    # pylint: disable=global-statement
+    global TRACER
+    TRACER = EventTracer(env)
+
+    # Uncomment the following line so that you can print
+    # the event history with TRACER.print_trace_data()
+
+    # TRACER.trace()
+
+    ###############################
 
     env.run()
 
 
 if __name__ == "__main__":
+    environ['LOGURU_FORMAT'] = (
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+        "<level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+        "<level>[{extra[component]: ^8}] {message}</level>")
+
+    from loguru import logger
+
     logger.add("output/data.log",
-               level='SUCCESS',
-               format="[{extra[platform]: <8} @ {extra[by_processor]: <2}]" +
-               " <level>{message}</level>",
+               level='TRACE',
+               filter=lambda record: 'by_processor' in record['extra'],
+               format=("[{extra[component]: <8} @ {extra[by_processor]: <2}]"
+                       " <level>{message}</level>"),
                enqueue=True)
 
     # selectivity: range is [0, 1]
@@ -214,4 +344,5 @@ if __name__ == "__main__":
                                    'id, length, ret_isp, submit_timestamp')
     DataPacket = namedtuple('DataPacket', 'command_id, length')
 
-    main()
+    with logger.catch():
+        main()
